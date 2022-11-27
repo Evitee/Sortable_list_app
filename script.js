abf04ab153c7ab000496419afae584d68aca2ac0
Evitee <evija.tetina@gmail.com>
2022-11-27 23:33:59 +0200

    feat: adds drag and drop functionality

--- script.js
+++ script.js
@@ -43,4 +43,60 @@ function createList() {
 
         draggable_list.appendChild(listItem);
     })
+    addEventListener();
+}
+
+function dragStart () {
+    //console.log('Event: ', 'dragstart');
+    dragStartIndex = this.closest('li').getAttribute('data-index');
+    console.log(dragStartIndex);
+}
+
+function dragEnter (){
+    //console.log('Event: ', 'dragenter');
+    this.classList.add('over');
+}
+
+
+function dragLeave (){
+   //console.log('Event: ', 'dragleave');
+   this.classList.remove('over');
+}
+
+function dragOver (e){
+   // console.log('Event: ', 'dragover');
+   e.preventDefault();
+}
+
+function dragDrop (){
+   //console.log('Event: ', 'dragdrop');
+   const dragEndIndex = +this.getAttribute('data-index');
+
+   swapItems(dragStartIndex, dragEndIndex);
+
+   this.classList.remove('over');
+}
+
+function swapItems(fromIndex, toIndex){
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+   listItems[fromIndex].appendChild(itemTwo);
+   listItems[toIndex].appendChild(itemOne);
+}
+
+function addEventListener() {
+    const draggables = document.querySelectorAll('.draggable');
+    const dragListItems = document.querySelectorAll('.draggable-list li');
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', dragStart);
+    });
+
+    dragListItems.forEach(item => {
+        item.addEventListener('dragover', dragOver);
+        item.addEventListener('drop', dragDrop);
+        item.addEventListener('dragenter', dragEnter);
+        item.addEventListener('dragleave', dragLeave);
+    })
 }
