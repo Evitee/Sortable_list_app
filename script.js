222defc3310c8f4dc27464e5d6c123c0c5243aa8
Evitee <evija.tetina@gmail.com>
2022-11-27 23:42:16 +0200

    feat: adds check functionality

--- script.js
+++ script.js
@@ -85,6 +85,20 @@ function swapItems(fromIndex, toIndex){
    listItems[toIndex].appendChild(itemOne);
 }
 
+function checkOrder(){
+    listItems.forEach((listItem, index) => {
+        const personName = listItem.querySelector('.draggable').innerText.trim();
+
+        if(personName !== richestPeople[index]) {
+            listItem.classList.add('wrong');
+        } else {
+            listItem.classList.remove('wrong');
+            listItem.classList.add('right');
+        }
+
+    })
+}
+
 function addEventListener() {
     const draggables = document.querySelectorAll('.draggable');
     const dragListItems = document.querySelectorAll('.draggable-list li');
@@ -100,3 +114,5 @@ function addEventListener() {
         item.addEventListener('dragleave', dragLeave);
     })
 }
+
+check.addEventListener('click', checkOrder);
