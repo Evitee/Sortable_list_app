6101ab5aa87e36e5982f443123a325b626985e38
Evitee <evija.tetina@gmail.com>
2022-11-27 22:55:13 +0200

    feat: adds styles

--- script.js
+++ script.js
@@ -21,8 +21,13 @@ let dragStartIndex;
 createList();
 
 function createList() {
-    [...richestPeople].forEach((person, index) => {
+    [...richestPeople]
+        .map(a => ({value: a, sort: Math.random() }))
+        .sort((a,b) => a.sort - b.sort)
+        .map(a => a.value)
+        .forEach((person, index) => {
         const listItem = document.createElement('li');
+
 
         listItem.setAttribute('data-index', index);
 
@@ -30,7 +35,7 @@ function createList() {
         <span class="number">${index + 1}</span>
         <div class="draggable" draggable = "true">
             <p class="person-name">${person}</p>
-            <i class="fa-light fa-grip-lines"></i>
+            <i class="fa-solid fa-grip-lines"></i>
         </div>
         `;
 
